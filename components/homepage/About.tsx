a6b8d8406bf3153c727ad1a5bc5aec3905b4110b
Khalid Adan <KhalidAdan@users.noreply.github.com>
2023-08-24 23:24:16 -0400

    Update About.tsx

--- components/homepage/About.tsx
+++ components/homepage/About.tsx
@@ -12,7 +12,7 @@ export default function About({ variants }: { variants: Variants }) {
       </TypographyH1>
       <TypographyLead>
         I&apos;m Khalid Adan, a software developer working out of Ottawa,
-        Canada. I&apos;ve lead software teams and built products for start-ups,
+        Canada. I&apos;ve led software teams and built products for start-ups,
         huge enterprises and the public sector. Currently, I&apos;m hacking on a
         product called Skillbox, and hope to show something soon!
       </TypographyLead>
